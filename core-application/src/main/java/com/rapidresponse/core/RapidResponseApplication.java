3d04743e42a0e7c13331a95b1b0c24fcb50ca604
NethmiDias <nethu200304202@gmail.com>
2026-08-27 22:55:10 +0530

    Update module route optimization

--- core-application/src/main/java/com/rapidresponse/core/RapidResponseApplication.java
+++ core-application/src/main/java/com/rapidresponse/core/RapidResponseApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(scanBasePackages = "com.rapidresponse")
+@SpringBootApplication
+@ComponentScan(basePackages = "com.rapidresponse",
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = com.rapidresponse.route.ModuleRouteOptimizationApplication.class))
 @EntityScan(basePackages = "com.rapidresponse.shared.entity")
 @EnableJpaRepositories(basePackages = "com.rapidresponse.shared.repository")
 public class RapidResponseApplication {
--- module-route-optimization/src/main/java/com/rapidresponse/route/ModuleRouteOptimizationApplication.java
+++ module-route-optimization/src/main/java/com/rapidresponse/route/ModuleRouteOptimizationApplication.java
@@ -2,8 +2,12 @@ package com.rapidresponse.route;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.rapidresponse")
+@EntityScan(basePackages = "com.rapidresponse.shared.entity")
+@EnableJpaRepositories(basePackages = "com.rapidresponse.shared.repository")
 public class ModuleRouteOptimizationApplication {
 
 	public static void main(String[] args) {

b26423b0331c94b0ebde4a028e087a8f2f66d49b
Niviru I.U. Weerasinghe <niviruedu2006@gmail.com>
2026-08-28 17:47:25 +0530

    build: configure route-optimization pom structure and add network-analysis dummy starter class

---
+++ module-network-analysis/src/main/java/com/rapidresponse/network/ModuleNetworkAnalysisApplication.java
@@ -0,0 +1,11 @@
+package com.rapidresponse.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleNetworkAnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleNetworkAnalysisApplication.class, args);
+    }
+}
